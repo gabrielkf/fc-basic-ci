eab110dbf45a389d5a8200da4a3f7277bb64f2a9
gabriel.paiva <gabriel.paiva@cloudkitchens.com>
2024-11-09 22:27:05 -0300

    feat: multiplication method

--- math.go
+++ math.go
@@ -9,3 +9,7 @@ func main() {
 func soma(a int, b int) int {
 	return a + b
 }
+
+func mult(a int, b int) int {
+	return a * b
+}
--- math_test.go
+++ math_test.go
@@ -10,3 +10,13 @@ func TestSoma(t *testing.T) {
 		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 30)
 	}
 }
+
+func TestMult(t *testing.T) {
+
+	actual := mult(3, 4)
+	expected := 10
+
+	if actual != expected {
+		t.Errorf("Expected %d, received %d", expected, actual)
+	}
+}
